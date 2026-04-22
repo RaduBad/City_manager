c8863b8b94c82d6ef4d0860a3bee03ce56d8631c
RaduBad <134852354+RaduBad@users.noreply.github.com>
2026-04-22 11:33:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include<string.h>
 #include<time.h>
 #include<sys/stat.h>
+#include<fcntl.h>
+#include <dirent.h>
 
 #define SIZE 30
 
@@ -29,7 +31,26 @@ typedef struct{
     int value;
 }COMMANDS;
 
-void add(char* district_id){
+void add_files(char* district_id){
+    open("reports.dat",O_APPEND | O_CREAT | O_WRONLY ,0664);
+    chmod("reports.dat",0664);
+    open("district.cfg",O_APPEND | O_CREAT | O_WRONLY ,0640);
+    chmod("district.cfg",0640);
+    open("logged_district",O_APPEND | O_CREAT | O_WRONLY ,0644);
+    chmod("logged_district",0644);
+}
+
+void add(char* district_id,char* user){
+    char path[256];
+    mkdir(district_id,0750);
+    chmod(district_id, 0750);
+    snprintf(path, sizeof(path), "%s/reports.dat", district_id);
+    open(path,O_APPEND | O_CREAT | O_WRONLY ,0664);
+    chmod("reports.dat",0664);
+    REPORT* r=malloc(sizeof(REPORT));
+    strcpy(r->inspector_name,user);
+    printf("ID report :");
+    scanf("%d",&r->ID);
 
 }
 
@@ -50,6 +71,7 @@ void filter(char* district_id, int value){
 
 int main(int argc, char**argv){
 
+
     COMMANDS com;
     for (int i = 0 ; i < argc ; i++){
         if(strcmp(argv[i],"--role")==0)
@@ -57,10 +79,11 @@ int main(int argc, char**argv){
         if(strcmp(argv[i],"--user")==0)
             strcpy(com.user,argv[i+1]);
         if(strcmp(argv[i],"--add")==0)
-            add();
-        if(strcmp(argv[i],"--list")==0)
-            list();
-        if(strcmp(argv[i],"--list")==0)
+            add(argv[++i],com.user);
+        //if(strcmp(argv[i],"--list")==0)
+           // list();
+
+
 
 
     }
